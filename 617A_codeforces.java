f69f95cd64da3c61c48b02c3a33551ee7010c092
manantageja7 <manantageja7@gmail.com>
2026-07-18 01:42:53 +0530

    Create 617A_codeforces.java

---
+++ 617A_codeforces.java
@@ -0,0 +1,37 @@
+// A. Elephant
+// time limit per test1 second
+// memory limit per test256 megabytes
+// An elephant decided to visit his friend. It turned out that the elephant's house is located at point 0 and his friend's house is located at point x(x > 0) of the coordinate line. In one step the elephant can move 1, 2, 3, 4 or 5 positions forward. Determine, what is the minimum number of steps he need to make in order to get to his friend's house.
+// Input
+// The first line of the input contains an integer x (1 ≤ x ≤ 1 000 000) — The coordinate of the friend's house.
+// Output
+// Print the minimum number of steps that elephant needs to make to get from point 0 to point x.
+// Examples
+// InputCopy
+// 5
+// OutputCopy
+// 1
+// InputCopy
+// 12
+// OutputCopy
+// 3
+// Note
+// In the first sample the elephant needs to make one step of length 5 to reach the point x.
+// In the second sample the elephant can get to point x if he moves by 3, 5 and 4. There are other ways to get the optimal answer but the elephant cannot reach x in less than three moves
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x=sc.nextInt();
+        if(x>5 && x%5!=0){
+            System.out.println((x/5)+1);
+        }
+        else if(x>5 && x%5==0){
+            System.out.println(x/5);
+        }
+        else{
+            System.out.println('1');
+        }
+        sc.close();
+    }
+}
