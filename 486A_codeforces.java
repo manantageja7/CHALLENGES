bcc3d4b038975f7448f1d8588bb800b147f9ed2a
manantageja7 <manantageja7@gmail.com>
2026-08-06 14:05:23 +0530

    Create 486A_codeforces.java

---
+++ 486A_codeforces.java
@@ -0,0 +1,38 @@
+// A. Calculating Function
+// For a positive integer n let's define a function f:
+
+// f(n) =  - 1 + 2 - 3 + .. + ( - 1)nn
+
+// Your task is to calculate f(n) for a given integer n.
+
+// Input
+// The single line contains the positive integer n (1 ≤ n ≤ 1015).
+
+// Output
+// Print f(n) in a single line.
+
+// Examples
+// InputCopy
+// 4
+// OutputCopy
+// 2
+// InputCopy
+// 5
+// OutputCopy
+// -3
+// Note
+// f(4) =  - 1 + 2 - 3 + 4 = 2
+
+// f(5) =  - 1 + 2 - 3 + 4 - 5 =  - 3
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        if (n % 2 == 0) {
+            System.out.println(n / 2);
+        } else {
+            System.out.println(-(n + 1) / 2);
+        }
+    }
+}
